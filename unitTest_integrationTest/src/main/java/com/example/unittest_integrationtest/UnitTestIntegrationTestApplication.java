ce8697dc7511a160d45b54e188d376c33c6d50cf
minseon33 <109520441+minseon33@users.noreply.github.com>
2024-12-16 14:18:50 +0900

    단위테스트 vs 통합테스트 연습코드 초기세팅

---
+++ unitTest_integrationTest/src/main/java/com/example/unittest_integrationtest/UnitTestIntegrationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.unittest_integrationtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitTestIntegrationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnitTestIntegrationTestApplication.class, args);
+    }
+
+}
---
+++ unitTest_integrationTest/src/test/java/com/example/unittest_integrationtest/UnitTestIntegrationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittest_integrationtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestIntegrationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
